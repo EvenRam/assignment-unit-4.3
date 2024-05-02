25a96e29af0d0f484d55ae932129551544ebdafb
crystal <crystalevenram@gmail.com>
2024-05-02 16:26:41 -0500

    working stetch goal

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,56 +3,108 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 
-// - Create a global variable named `basket` and set it to an empty array.
+
+// - Create a variable named `basket` and set it to an empty array.
 
 // - Create a function called `addItem`. It should:
 //   - take an input parameter for a string `item`
 //   - add the new item to the global array `basket`. 
+//          --- we want 'basket' and push the items into it
 //   - return `true` indicating the item was added
+ //       -- return true 
+
+ const maxItems =5;
+
+let basket = [];
+//console.log(basket);
+
+// 3. ( Stretch Goal) Update the required `addItem` function to:
+//   - Use the `isFull` function to prevent more than `maxItems` from being added to the basket. 
+//   - If an item was added to the array, return `true`
+//   - If there was no room and the item could not be added return `false`
+
+
+/** 
+ * call function isFull
+ * create an if statment 
+ *  if 
+ */
+function addItem(item){
+    basket.push(item)
+     isFull(){
+        //console.log(basket);
+         return true;
+    }
+}
+
+// orginal code from question 2. 
+// function addItem(item){
+//     basket.push(item)
+//     //console.log(basket);
+//     return true;
+// }
+
+// console.log ---- call function amd add the arguements 
+//output--- we are  using the function to add the milk, egg and bead to the basket
+// return true 
+console.log(addItem(['milk','egg', 'bread']));
 
 // - Create a function called `listItems`. It should:
 //   - loop over the items in the `basket` array
 //   - console.log each individual item on a new line
 
+    function listItems(){
+        for(let i = 0; i < basket.length; i++){
+            console.log (basket[i]);
+        }
+    }
+
 // - Create a function called `empty`. It should:
 //   - reset the `basket` to an empty array
 //   - do not use `basket = []` to reset the array
 
-//
+    listItems(basket);
 
-let basket = [];
-function addItem(item){
-    basket.push(item)
-    return true;
-}
-console.log(addItem(['milk','egg', 'bread']));
+    function empty(){
+        basket.length = 0;
+    console.log(basket);
 
-
-    function listItems(){
-        for(let i = 0; i < basket.length; i++){
-            console.log (basket[i]);
-
-
-        }
-
-    }
-
-    listItems();
-
-    // function empty(){
-    //     basket.length = 0;
-    //    }
+       }
 
 // try while loop
-function empty(){
-    while(basket.length > 0){
-        basket.pop()
-        console.log(basket);
-}
+// function empty(){
+//     while(basket.length > 0){
+//         basket.pop()
+//         console.log(basket);
+// }
 
-}
+// }
 
 empty();
+
+// 2.(stratch Goal) 
+//Create a function called isFull(). It should:
+//   - return `false` if the basket contains *less* than max number of items ---basket < maxitems.length
+/**if statement:
+ *  if basket < maxitems
+ * return false
+ */
+//   - return `true` otherwise (equal or more than maxItems)
+
+function isFull(){
+    if( basket < maxItems ){
+        console.log(basket);
+    return false
+    } else {
+        return true
+    }
+}
+
+console.log("is the basket full ", isFull());
+// ### Stretch Goals 
+
+
+// 1. Add a global `const` named `maxItems` and set it to 5.
 
 
 
